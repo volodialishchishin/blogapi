1bf4aaa8f150961ccd8fd81a2d142460f0df3051
yuraba <yurikulikov22@gmail.com>
2022-11-10 19:16:58 +0200

    fixds4234324

--- src/middlwares/auth-middleware.ts
+++ src/middlwares/auth-middleware.ts
@@ -2,6 +2,9 @@ import {NextFunction, Request, Response} from "express";
 
 export const authMiddleware = ((req:Request, res:Response, next:NextFunction) => {
     const auth = {login: 'admin', password: 'qwerty'}
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0]!== 'Basic'){
+        res.sendStatus(401)
+    }
     const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
     const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
     if (login && password && login === auth.login && password === auth.password) {
