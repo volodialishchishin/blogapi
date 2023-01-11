dd8a871fc31e734161b20e6e947238494b92b99b
yuraba <yurikulikov22@gmail.com>
2023-01-11 08:08:20 +0200

    HW 9 seventh

--- src/routes/security-router.ts
+++ src/routes/security-router.ts
@@ -25,8 +25,7 @@ securityRouter.delete('/devices',
             let deleteResult = await securityService.deleteSessions(refreshToken)
             if (deleteResult.deletedCount) {
                 res.sendStatus(204)
-            }
-            else{
+            } else {
                 res.sendStatus(401)
             }
 
@@ -40,25 +39,20 @@ securityRouter.delete('/devices/:id',
     async (req: Request, res: Response) => {
         const {refreshToken} = req.cookies;
         try {
-            const { user } = <jwt.UserIDJwtPayload>jwt.verify(refreshToken, process.env.SECRET || 'Ok')
-            await securityRepository.getSession(user,req.body.id)
-        }
-        catch (e) {
-            res.sendStatus(403)
+            const {user} = <jwt.UserIDJwtPayload>jwt.verify(refreshToken, process.env.SECRET || 'Ok')
+            await securityRepository.getSession(user, req.body.id)
+        } catch (e) {
+            return res.sendStatus(403)
         }
         try {
-
-
             let deleteResult = await securityService.deleteSession(refreshToken, req.body.id)
             if (deleteResult.deletedCount) {
                 res.sendStatus(204)
             } else {
                 res.sendStatus(404)
             }
-        } catch (e:any) {
-
-                res.sendStatus(401)
-
+        } catch (e) {
+            res.sendStatus(401)
         }
 
     })
--- src/services/security-service.ts
+++ src/services/security-service.ts
@@ -3,23 +3,18 @@ import jwt from "jsonwebtoken";
 import {DeleteResult} from "mongodb";
 
 export const securityService = {
-    async getSessions(refreshToken:string) {
-        const { user } = <jwt.UserIDJwtPayload>jwt.verify(refreshToken, process.env.SECRET || 'Ok')
+    async getSessions(refreshToken: string) {
+        const {user} = <jwt.UserIDJwtPayload>jwt.verify(refreshToken, process.env.SECRET || 'Ok')
         return await securityRepository.getSessions(user)
     },
-    async deleteSessions(refreshToken:string) {
-        const { user, deviceId } = <jwt.UserIDJwtPayload>jwt.verify(refreshToken, process.env.SECRET || 'Ok')
+    async deleteSessions(refreshToken: string) {
+        const {user, deviceId} = <jwt.UserIDJwtPayload>jwt.verify(refreshToken, process.env.SECRET || 'Ok')
         return await securityRepository.deleteSessions(user, deviceId)
     },
-    async deleteSession(refreshToken:string, id:string): Promise<DeleteResult> {
+    async deleteSession(refreshToken: string, id: string): Promise<DeleteResult> {
 
-        const { user } = <jwt.UserIDJwtPayload>jwt.verify(refreshToken, process.env.SECRET || 'Ok')
-        try {
-            await securityRepository.getSession(user,id)
-        }
-        catch (e) {
-            throw new Error('User with this id can not delete this session')
-        }
-        return await securityRepository.deleteSession(user,id)
+        const {user} = <jwt.UserIDJwtPayload>jwt.verify(refreshToken, process.env.SECRET || 'Ok')
+        return await securityRepository.deleteSession(user, id)
+
     }
 }
