dc1a94485131471d43ee32bec1ef40a9b8c2baee
yuraba <yurikulikov22@gmail.com>
2023-01-15 21:06:54 +0200

    HW 10 second try

--- src/services/mail-service.ts
+++ src/services/mail-service.ts
@@ -16,5 +16,19 @@ export const mailService = {
             html: `<a href=${url}>${url}</a>`
         });
 
+    },
+    async sendRecoveryPasswordCode(user: UserModel, resend:boolean = false,newCode:string='') {
+        console.log(user)
+        let url = `https://somesite.com/confirm-email?recoveryCode=${resend?newCode:user.emailConfirmation.confirmationCode}`
+
+        console.log(url)
+        let info = await transporter.sendMail({
+            from: 'Volodia',
+            to: user.accountData.email,
+            subject: "Confirm your content",
+            text: "Confirm your account",
+            html: `<a href=${url}>${url}</a>`
+        });
+
     }
 }
